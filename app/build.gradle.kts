11a3563686ae761d5a68419c426dfb5da747c0dd
JeetN <get2jeetnegi@gmail.com>
2024-06-24 02:37:24 +0530

    Default Splash Screen Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Splash API
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
--- app/src/main/java/com/diagonal/watchhub/MainActivity.kt
+++ app/src/main/java/com/diagonal/watchhub/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,11 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.diagonal.watchhub.ui.theme.WatchhubTheme
+import com.diagonal.watchhub.ui.viewmodel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // add it before setContent
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
             WatchhubTheme {
